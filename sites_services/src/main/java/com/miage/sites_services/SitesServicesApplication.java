a7449e3eff64446eeb24c930d3c3d8dfb85af204
zw-2001 <58522079+zw-2001@users.noreply.github.com>
2024-06-05 03:19:08 +0200

    Setup project

---
+++ sites_services/src/main/java/com/miage/sites_services/SitesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.miage.sites_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitesServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitesServicesApplication.class, args);
+    }
+
+}
---
+++ sites_services/src/test/java/com/miage/sites_services/SitesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.sites_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitesServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
