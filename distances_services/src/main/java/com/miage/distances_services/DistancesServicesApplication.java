741a2633126b9083b8d1cb4655a674b944395b83
zw-2001 <58522079+zw-2001@users.noreply.github.com>
2024-06-05 19:19:33 +0200

    Init distances_services

---
+++ distances_services/src/main/java/com/miage/distances_services/DistancesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.miage.distances_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistancesServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistancesServicesApplication.class, args);
+    }
+
+}
