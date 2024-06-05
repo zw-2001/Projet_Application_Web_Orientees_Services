61a0d9f8b4a98f8007735a56a789a52f934781a3
zw-2001 <58522079+zw-2001@users.noreply.github.com>
2024-06-05 23:32:12 +0200

    Neo4j

---
+++ distances_services/src/main/java/com/miage/distances_services/entity/Site.java
@@ -0,0 +1,63 @@
+package com.miage.distances_services.entity;
+
+import com.miage.distances_services.entity.Event;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import java.util.Set;
+
+@Node
+public class Site {
+    @Id
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    @Relationship(type = "LOCATED_AT", direction = Relationship.Direction.INCOMING)
+    private Set<Event> events;
+
+    @Relationship(type = "DISTANCE_TO")
+    private Set<Distance> distances;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+
+    public Set<Distance> getDistances() {
+        return distances;
+    }
+
+    public void setDistances(Set<Distance> distances) {
+        this.distances = distances;
+    }}
+
+
