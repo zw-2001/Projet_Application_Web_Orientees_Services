efe6e8f00d6333c143e51c4e2399ae6b6e73f8d8
zw-2001 <58522079+zw-2001@users.noreply.github.com>
2024-06-05 16:03:12 +0200

    Init calendar_services

---
+++ calendar_services/src/main/java/com/miage/calendar_services/CalendarServicesApplication.java
@@ -0,0 +1,13 @@
+package com.miage.calendar_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarServicesApplication.class, args);
+    }
+
+}
---
+++ calendar_services/src/test/java/com/miage/calendar_services/CalendarServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.calendar_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
